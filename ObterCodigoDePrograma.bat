5831bfcfa4a7d2cc7fe582b280091dc412aa5480
Rogério Alves <rogerio.alves651@gmail.com>
2025-05-30 11:07:20 -0300

    Corrigir mensagem de saída e ordem dos parâmetros no script para obter códigos, nomes e versões dos produtos instalados em computadores remotos.

--- ObterCodigoDePrograma.bat
+++ ObterCodigoDePrograma.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p RemotePC="Digite o nome ou IP do computador remoto: "
-echo Obtendo codigos de produto instalados em %RemotePC%...
-wmic /node:%RemotePC% product get name,version identifyingnumber
+echo Obtendo códigos, nomes e versões dos produtos instalados em %RemotePC%...
+wmic /node:%RemotePC% product get name, identifyingnumber, version
 pause
