ab018dec594398ae849bf9b47c18f6bc7fa08f7d
Rogério Alves <rogerio.alves651@gmail.com>
2025-05-30 11:08:04 -0300

    Corrigir acentuação na mensagem de saída do script para obter códigos, nomes e versões dos produtos instalados em computadores remotos.

--- ObterCodigoDePrograma.bat
+++ ObterCodigoDePrograma.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p RemotePC="Digite o nome ou IP do computador remoto: "
-echo Obtendo códigos, nomes e versões dos produtos instalados em %RemotePC%...
+echo Obtendo codigos, nomes e versoes dos produtos instalados em %RemotePC%...
 wmic /node:%RemotePC% product get name, identifyingnumber, version
 pause
