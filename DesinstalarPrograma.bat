cd6bfa078158ad05fc4b2be6ba86c4d0b4b04651
Rogério Alves <rogerio.alves651@gmail.com>
2025-05-31 19:28:50 -0300

    Corrigir chamada do psexec no script de desinstalação de programas em computadores remotos.

--- DesinstalarPrograma.bat
+++ DesinstalarPrograma.bat
@@ -3,7 +3,7 @@ set /p RemotePC="Digite o nome ou IP do computador remoto: "
 set /p ProductCode="Digite o código de identificação do produto: "
 
 echo Conectando a %RemotePC% e desinstalando o produto com código %ProductCode%...
-psexec \\%RemotePC% cmd /c "wmic product where identifyingnumber='%ProductCode%' call uninstall /nointeractive"
+{6C0A3C5E-7725-49D8-A016-B3ADCACF61C2}psexec \\%RemotePC% cmd /c wmic product where identifyingnumber^='%ProductCode%' call uninstall /nointeractive
 
 echo Processo concluído!
 pause
